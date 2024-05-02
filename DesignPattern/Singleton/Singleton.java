39c3ba783b6ce06ee54efe74892c24ffa4d33893
Harsh Kumar <vijayk4378@gmail.com>
2024-05-02 21:54:15 +0530

    Design Pattern Started

---
+++ DesignPattern/Singleton/Singleton.java
@@ -0,0 +1,11 @@
+package DesignPattern.Singleton;
+
+/**
+ * Singleton
+ */
+public class Singleton {
+
+    public static void main(String[] args) {
+        System.out.println("Design pattern");
+    }
+}
